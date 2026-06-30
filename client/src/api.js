93385682d857e2b22a194acb2552b03f528d7d5f
harinimurugesan23 <harinimurugesan23@gmail.com>
2026-06-30 10:49:33 -0700

    Configure frontend API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: import.meta.env.DEV
+    ? "/api"
+    : import.meta.env.VITE_API_URL,
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
